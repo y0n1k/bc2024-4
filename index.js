53be25d72a41f48a9fba2d7adeb7b0289515fc57
Пекарський Ян bc2024-4 <yan.pekarski@gmail.com>
2024-10-15 16:27:28 +0300

    get added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
 const { Command } = require('commander');
 const program = new Command();
 
 program
-  .requiredOption('-h, --host <host>', 'Server host')
-  .requiredOption('-p, --port <port>', 'Server port')
-  .requiredOption('-c, --cache <path>', 'Cache directory path');
+.requiredOption('-h, --host <host>', 'Server host')
+.requiredOption('-p, --port <port>', 'Server port')
+.requiredOption('-c, --cache <path>', 'Cache directory path');
 
 program.parse(process.argv);
 
@@ -15,12 +17,30 @@ console.log(`Host: ${host}`);
 console.log(`Port: ${port}`);
 console.log(`Cache directory: ${cache}`);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Done\n');
-});
+const server = http.createServer(async (req, res) => {
+const urlParts = req.url.split('/');
+const httpCode = urlParts[1];
+const filePath = path.join(cache, `${httpCode}.jpg`);
 
+if (req.method === 'GET') {
+try {
+const image = await fs.readFile(filePath);
+res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+res.end(image);
+} catch (error) {
+if (error.code === 'ENOENT') {
+res.writeHead(404, { 'Content-Type': 'text/plain' });
+res.end('Not Found');
+} else {
+res.writeHead(500, { 'Content-Type': 'text/plain' });
+res.end('Internal Server Error');
+}
+}
+} else {
+res.writeHead(405, { 'Content-Type': 'text/plain' });
+res.end('Method Not Allowed');
+}
+});
 server.listen(port, host, () => {
-  console.log(`http://${host}:${port}/`);
+console.log(`http://${host}:${port}/`);
 });
