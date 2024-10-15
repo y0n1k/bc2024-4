76ec34569f1cc4ee5909f92de42a7e513ef2e0fd
Пекарський Ян bc2024-4 <yan.pekarski@gmail.com>
2024-10-15 16:29:11 +0300

    put added

--- index.js
+++ index.js
@@ -3,25 +3,19 @@ const fs = require('fs').promises;
 const path = require('path');
 const { Command } = require('commander');
 const program = new Command();
-
 program
 .requiredOption('-h, --host <host>', 'Server host')
 .requiredOption('-p, --port <port>', 'Server port')
 .requiredOption('-c, --cache <path>', 'Cache directory path');
-
 program.parse(process.argv);
-
 const { host, port, cache } = program.opts();
-
 console.log(`Host: ${host}`);
 console.log(`Port: ${port}`);
 console.log(`Cache directory: ${cache}`);
-
 const server = http.createServer(async (req, res) => {
 const urlParts = req.url.split('/');
 const httpCode = urlParts[1];
 const filePath = path.join(cache, `${httpCode}.jpg`);
-
 if (req.method === 'GET') {
 try {
 const image = await fs.readFile(filePath);
@@ -36,6 +30,19 @@ res.writeHead(500, { 'Content-Type': 'text/plain' });
 res.end('Internal Server Error');
 }
 }
+} else if (req.method === 'PUT') {
+let body = [];
+req.on('data', chunk => body.push(chunk));
+req.on('end', async () => {
+try {
+await fs.writeFile(filePath, Buffer.concat(body));
+res.writeHead(201, { 'Content-Type': 'text/plain' });
+res.end('Created');
+} catch (error) {
+res.writeHead(500, { 'Content-Type': 'text/plain' });
+res.end('Internal Server Error');
+}
+});
 } else {
 res.writeHead(405, { 'Content-Type': 'text/plain' });
 res.end('Method Not Allowed');
